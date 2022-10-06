b2f1db539195808c077533669c679ae96a0fa398
FedericoTossani <84522543+FedericoTossani@users.noreply.github.com>
2022-10-06 14:55:00 +0200

    Create R_first_code.r

---
+++ R_first_code.r
@@ -0,0 +1,10 @@
+# this is the first code in github wrote during monitoring ecosystem changes and functioning course
+# because I am currently working I will be following the 2021/2022 lessons recorded on Panopto
+
+# here are the first input data, from Costanza who collected streams' water and from Marta who studied fishs genome
+water <- c(100, 200, 300, 400, 500)
+fishes <- c(10, 50, 60, 100, 200)
+
+#plot is the base function of R with which one can plot variables
+plot(water, fishes)
+
