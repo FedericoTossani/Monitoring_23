3e69d52a60689a0971fbf3a1163ffaa7d0b40c55
FedericoTossani <84522543+FedericoTossani@users.noreply.github.com>
2022-11-17 16:55:06 +0100

    Create R_code_sdm_22.r

---
+++ R_code_sdm_22.r
@@ -0,0 +1,13 @@
+# R code for species distribution modelling
+
+# Packages we need
+
+library(raster)
+library(rgdal)
+library(sdm)
+
+# Set the working directory we are working on
+setwd("C:/lab/")
+
+# system.file() is the function to use to inspect the file contained in a package!
+
