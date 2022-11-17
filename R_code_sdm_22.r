f0dc03e9c806a0b1b72601ca09efebe9b544e31c
FedericoTossani <84522543+FedericoTossani@users.noreply.github.com>
2022-11-17 19:13:49 +0100

    Update R_code_sdm_22.r

--- R_code_sdm_22.r
+++ R_code_sdm_22.r
@@ -12,7 +12,7 @@ setwd("C:/lab/")
 # system.file() is the function to use to inspect the file contained in a package!
 file <- system.file("external/species.shp", package = "sdm")
 
-# shapefile() function does the job as raster but for .shp files
+# shapefile() function does the job as raster() do but for .shp files
 species <- shapefile(file)
 
 plot(species, pch=19, col="dark green")
